2d2c7773539048c1c15769a596ed880b16afe613
seanred360 <41050673+seanred360@users.noreply.github.com>
2019-05-10 21:36:08 +0800

    First Commit

---
+++ Assets/Ore.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ore : MonoBehaviour
+{
+    float m_health;
+    public PlayerController m_playerController;
+    public GameObject rockParticlePrefab;
+
+    void Awake()
+    {
+        m_playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        m_health = 3;
+    }
+
+    void Update()
+    {
+        if (m_health <= 0)
+        {
+            DestroyOre();
+        }
+    }
+
+    public void StartMineOre()
+    {
+        StartCoroutine(MineOre());
+    }
+
+    IEnumerator MineOre()
+    {
+        yield return new WaitForSeconds(1);
+        HitRock();
+        StartCoroutine(MineOre());
+    }
+
+    void HitRock()
+    {
+        GameObject clone;
+        clone = Instantiate(rockParticlePrefab, transform.position, transform.rotation);
+        m_health -= 1;
+    }
+    void DestroyOre()
+    {
+        m_playerController.m_canMove = true;
+        m_playerController.StopMining();
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    bool m_isMoving;
+    public bool m_canMove = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetMouseButtonDown(0)) && m_canMove)
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                m_isMoving = true;
+                agent.SetDestination(hit.point + new Vector3(0, 0, 2));
+            }
+            else Debug.Log("no possible paths");
+            if (hit.collider.tag == "Ore")
+            {
+                StartCoroutine(StartMining(hit.collider.gameObject));
+                m_canMove = false;
+            }
+        }
+            if (!agent.pathPending)
+            {
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                    {
+                        m_isMoving = false;
+                        this.GetComponentInChildren<Animator>().SetBool("isWalking", false);
+                    }
+                }
+            }
+            if (m_isMoving == true)
+            {
+                this.GetComponentInChildren<Animator>().SetBool("isWalking", true);
+                this.GetComponentInChildren<Animator>().SetBool("isMining", false);
+            }
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Ore")
+        {
+            m_isMoving = false;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Ore")
+        {
+            m_isMoving = true;
+        }
+    }
+    IEnumerator StartMining(GameObject targetOre)
+    {
+        if(targetOre == null)
+        {
+            yield break;
+        }
+        while (m_isMoving == true)
+        { 
+              yield return null;
+        }
+        this.GetComponentInChildren<Animator>().SetBool("isMining", true);
+        targetOre.GetComponent<Ore>().StartMineOre();
+    }
+
+    public void StopMining()
+    {
+        this.GetComponentInChildren<Animator>().SetBool("isMining", false);
+    }
+}
